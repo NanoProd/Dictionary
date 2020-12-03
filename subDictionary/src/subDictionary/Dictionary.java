728a7fc9407b7dbf345b2d80bab66f1650e5adc8
Joshua-James Nantel-Ouimet <joshuajamesnano@gmail.com>
2020-12-03 12:23:44 -0500

    javadoc edit

--- subDictionary/src/subDictionary/Dictionary.java
+++ subDictionary/src/subDictionary/Dictionary.java
@@ -1,3 +1,15 @@
+//-------------------------------------------------
+//Written by: Joshua-James Nantel-Ouimet (#40131733)
+//            Samaninder Singh (#40133493)
+//COMP 249
+//Assignment 4
+//Due date: december 4th 2020
+//-------------------------------------------------
+/**
+ * @author Joshua-James Nantel-Ouimet
+ * @author Samaninder Singh 
+ * @version 1.0
+ */
 package subDictionary;
 
 import java.util.ArrayList;
